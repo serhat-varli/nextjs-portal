41b0fab33d9c5ee383f402aa291bfab691ce3dc7
Serhat Varlı <info@serhatvarli.com>
2023-04-26 14:11:00 +0300

    proje start

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    {
+      nav: [
+        {
+          name: "Show All Tyres",
+          url: "javascript:vodi(0);",
+          order: 1,
+        },
+        {
+          name: "Find a Dealer",
+          url: "javascript:vodi(0);",
+          order: 1,
+        },
+        {
+          name: "Guides & Videos",
+          url: "javascript:vodi(0);",
+          order: 1,
+        },
+        {
+          name: "Go with",
+          url: "javascript:vodi(0);",
+          order: 1,
+        },
+        {
+          name: "Service & Help",
+          url: "javascript:vodi(0);",
+          order: 1,
+        }
+      ],
+    }
+  )
 }
